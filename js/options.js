9b217213beea400823a35d838f0e805855df7c5e
dhlwing <249717835@qq.com>
2013-06-02 01:48:33 +0800

    fix bug

--- js/options.js
+++ js/options.js
@@ -1,1 +1,15 @@
 $(document).ready(init_options_page);
+var option = function(){this.init()};
+
+option.prototype.init = function(){
+	$('#link_about_plugin').click(function(){
+		$('#update_desc').modal({
+		    backdrop:true,
+		    keyboard:true,
+		    show:true
+		});
+	});
+	$('#button_about_plugin_close').click(function(){$('#update_desc').modal('hide');});
+}
+
+new option();
